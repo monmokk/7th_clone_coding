2fb3913088d5e72069f38c1fbcadb8bab8947342
monmokk <myeongjikang910814@gmail.com>
2022-06-17 11:49:05 +0900

    db - association 완성

--- models/order.js
+++ models/order.js
@@ -4,13 +4,15 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Order extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      this.belongsTo(models.User, {
+        foreignKey: 'userId',
+        targetKey: 'userId'
+      })
+      this.belongsTo(models.Restaurant, {
+        foreignKey: 'restaurantId',
+        targetKey: 'restaurantId'
+      })
     }
   }
   Order.init({
