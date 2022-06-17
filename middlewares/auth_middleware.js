0053ded2d4399fcba4638a8bc3740de960d5469b
monmokk <myeongjikang910814@gmail.com>
2022-06-17 22:40:05 +0900

    흑흑 제발 로그인 끝

--- middlewares/auth_middleware.js
+++ middlewares/auth_middleware.js
@@ -28,15 +28,19 @@ const checkLogin = (req, res, next) => {
 
 const checkAlreadyLogin = (req, res, next) => {
     const { authorization } = req.headers;
-    const [tokenType] = authorization.split(' ');
-
-    if (tokenType === 'Bearer') {
-        res.status(401).send({
-            errorMessage: '이미 로그인되어 있습니다',
-        });
+    if(typeof authorization === 'string'){
+        const [tokenType] = authorization.split(' ');
+        if (tokenType === 'Bearer') {
+            res.status(401).send({
+                errorMessage: '이미 로그인되어 있습니다',
+            });
+        } else {
+            next()
+        }
     } else {
         next()
     }
+
 }
 
 module.exports = {
