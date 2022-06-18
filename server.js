a8060d0cfc60a123960d39e61111c336ac1eb48e
monmokk <myeongjikang910814@gmail.com>
2022-06-19 01:46:55 +0900

    실시간 채팅 구현중

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const { server, app } = require('./app')
+const { server, io } = require('./app')
 const port = process.env.PORT;
+const { checkCounselor } = require('./middlewares/auth_middleware');
 
 server.listen(port || 3000, () => {
     console.log(`
@@ -7,4 +8,26 @@ server.listen(port || 3000, () => {
                     ${port} server run
         #########################################
     `)
+})
+
+const connectedUser = {}
+io.on('connection', (socket) => {
+
+    socket.on('token',token => {
+        const userId = checkCounselor(token)
+        connectedUser[userId] = socket.id
+        const userType = userId === 2? '상담사':'고객';
+        console.log(connectedUser, userType)
+        socket.emit('token', userType);
+    })
+    socket.on('chat', (data) => {
+        console.log(data);
+        socket.emit('chat', data.msg);
+
+    });
+    socket.on('error', (err) => {
+        console.log(`connect_error due to ${err.message}`);
+    });
+    socket.on('disconnect', () => {
+    });
 })
